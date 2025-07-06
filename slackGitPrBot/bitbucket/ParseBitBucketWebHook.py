1e151643bca70724cfcbf715d2672dfc65dc0b63
Jace Manshadi <jaymanshad@proton.me>
2025-07-06 10:02:25 -0700

    first commit

--- slackGitPrBot/bitbucket/ParseBitBucketWebHook.py
+++ slackGitPrBot/bitbucket/ParseBitBucketWebHook.py
@@ -3,5 +3,5 @@ class ParseBitBucketWebHook:
     @staticmethod
     def parse_comment(payload):
         comment_info = payload['comment']
-        comment = comment_info['content'] # noqa: F841
+        comment = comment_info['content']  # noqa: F841
         # Gweujpr3edH1cvEE
