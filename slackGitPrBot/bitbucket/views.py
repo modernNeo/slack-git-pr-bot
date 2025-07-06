4f6e3f94db8f6cfdb4c6bf902facd9067cffa7ef
Jace Manshadi <jaymanshad@proton.me>
2025-07-06 10:42:44 -0700

    first commit

--- slackGitPrBot/bitbucket/views.py
+++ slackGitPrBot/bitbucket/views.py
@@ -1,11 +1,9 @@
-import hashlib  # noqa: F401
-import hmac  # noqa: F401
 import json
 
 from django.http import HttpResponse
 from rest_framework import views
 
-# from bitbucket.ParseBitBucketWebHook import ParseBitBucketWebHook
+from bitbucket.ParseBitBucketWebHook import ParseBitBucketWebHook
 
 
 # Create your views here.
@@ -16,35 +14,20 @@ class Webhook(views.APIView):
         event_key = request.headers['X-Event-Key']
         print(json.dumps({**request.headers}, indent=4))
         print(f"X-Event-Key={event_key}")
-        # print(json.dumps(request.data, indent=4))
-        secret = "Gweujpr3edH1cvEE"  # noqa: F841
+        print(json.dumps(request.data, indent=4))
         print(1)
-        json_load = json.dumps(request.data)
         print(2)
-        enc_load = f"{json_load}".encode("utf-8")  # noqa: F841
         print(3)
-        # hash_object = hmac.new(  # noqa: F841
-        #     secret.encode("utf-8"),
-        #     msg=enc_load,
-        #     digestmod=hashlib.sha256,
-        # )
         print(4)
-        # calculated_signature = "sha256=" + hash_object.hexdigest()  # noqa: F841
         print(5)
-        given_signature = request.headers['X-Hub-Signature']  # noqa: F841
         print(6)
-        # if not hmac.compare_digest(calculated_signature, given_signature):
-        #     print(
-        #         "Signatures do not match\nExpected signature:"
-        #         f" {calculated_signature}\nActual: signature: {given_signature}"
-        #     )
-        # else:
-        #     print("Signatures match")
-        print(7)
-        # if event_key == "pullrequest:comment_created":
-        #     ParseBitBucketWebHook.parse_comment(request.data)
-        # elif event_key == 'pullrequest:changes_request_created':
-        #     pass
-        # elif event_key == 'pullrequest:approved':
-        #     pass
+        print(1)
+        if event_key == "pullrequest:comment_created":
+            ParseBitBucketWebHook.parse_comment(request.data)
+        elif event_key == 'pullrequest:changes_request_created':
+            pass
+        elif event_key == 'pullrequest:approved':
+            pass
         return HttpResponse("Hello, world. You're at the bitbucket webhook index.")
+
+
