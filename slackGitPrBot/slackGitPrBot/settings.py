5f7f83947a44f086ac422483be7055b4d365b335
Jace Manshadi <jaymanshad@proton.me>
2025-12-29 01:26:39 -0800

    forgot to install app

--- slackGitPrBot/slackGitPrBot/settings.py
+++ slackGitPrBot/slackGitPrBot/settings.py
@@ -34,8 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'bot_logging'
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
